3ea9bbda4aa221ea2fbba8485558268dc92bc9ed
CamiloDev404 <crosti1826@gmail.com>
2024-03-20 02:56:08 -0500

    Se realiza la construccion del componente footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Footer } from "./Components/Footer";
 import { Navbar } from "./Components/Navbar/Navbar";
 import './style.css';
 
@@ -6,6 +7,45 @@ export const App = () => {
     return (
         <>
             <Navbar/>
+            <p>as</p>
+            <p>as</p>
+            <p>as</p>
+            <p>as</p>
+            <p>as</p>
+            <p>as</p>
+            <p>as</p>
+            <p>as</p>
+            <p>as</p>
+            <p>as</p>
+            <p>as</p>
+            <p>as</p>
+            <p>as</p>
+            <p>as</p>
+            <p>as</p>
+            <p>as</p>
+            <p>as</p>
+            <p>as</p>
+            <p>as</p>
+            <p>as</p>
+            <p>as</p>
+            <p>as</p>
+            <p>as</p>
+            <p>as</p>
+            <p>as</p>
+            <p>as</p>
+            <p>as</p>
+            <p>as</p>
+            <p>as</p>
+            <p>as</p>
+            <p>as</p>
+            <p>as</p>
+            <p>as</p>
+            <p>as</p>
+            <p>as</p>
+            <p>as</p>
+            <p>as</p>
+            <p>as</p>
+            <Footer/>
         </>
     );
 
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,15 @@
 
 export const Footer = () => {
     return (
-        <>
-        </>
+        <div className="footer">
+            <div className="row">
+                <div className="col">
+                    <img src="/icon_gobierno.png" alt="icon_gobierno" style={{ marginTop: '20px' }}/>
+                </div>
+                <div className="col">
+                    <img src="/info.png" alt="icon_gobierno" style={{ marginTop: '20px', marginRight: '250px' }} />
+                </div>
+            </div>
+        </div>
     );
 }
