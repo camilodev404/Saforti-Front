90fd5c5f2654d3ca094b23e93f897afc00c62f19
CamiloDev404 <crosti1826@gmail.com>
2024-03-20 01:25:50 -0500

    Estructura inicial de paquetes y archivos jsx

---
+++ src/Pages/MenuUsuario.jsx
@@ -0,0 +1,7 @@
+
+export const MenuUsuario = () => {
+    return (
+        <>
+        </>
+    );
+}
---
+++ src/Pages/RegistroUsuario.jsx
@@ -0,0 +1,7 @@
+
+export const RegistroUsuario = () => {
+    return (
+        <>
+        </>
+    );
+}
