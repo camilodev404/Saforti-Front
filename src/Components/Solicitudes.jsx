3d728e2faa8b614af4cb5e47694613b8b9e5c904
CamiloDev404 <crosti1826@gmail.com>
2024-03-21 18:27:02 -0500

    Se estructura la base para listar las solicitudes del usuario

--- src/Components/Solicitudes.jsx
+++ src/Components/Solicitudes.jsx
@@ -2,8 +2,65 @@
 export const Solicitudes = () => {
     return (
         <> 
-            <div className="div-menu-solicitud">
-                <p>Listado de solicitudes</p>
+            <div className="div-menu-solicitud" >
+                <table className="table table-hover table-striped" style={{ borderColor: 'black', border: '2px solid #ccc'}}>
+                    <thead>
+                        <tr>
+                            <th>#Formulario</th>
+                            <th>Fecha</th>
+                            <th>Ver</th>
+                            <th>Editar</th>
+                            <th>Estado</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>0123</td>
+                            <td>01-02-2024</td>
+                            <td>
+                                <button className="btn" style={{ backgroundColor: '#037250', color: 'white' }}>
+                                    Ver
+                                </button>
+                            </td>
+                            <td>
+                                <button className="btn" style={{ backgroundColor: '#037250', color: 'white' }}>
+                                    Editar
+                                </button>
+                            </td>
+                            <td>Enviado</td>
+                        </tr>
+                        <tr>
+                            <td>0123</td>
+                            <td>01-02-2024</td>
+                            <td>
+                                <button className="btn" style={{ backgroundColor: '#037250', color: 'white' }}>
+                                    Ver
+                                </button>
+                            </td>
+                            <td>
+                                <button className="btn" style={{ backgroundColor: '#037250', color: 'white' }}>
+                                    Editar
+                                </button>
+                            </td>
+                            <td>Enviado</td>
+                        </tr>
+                        <tr>
+                            <td>0123</td>
+                            <td>01-02-2024</td>
+                            <td>
+                                <button className="btn" style={{ backgroundColor: '#037250', color: 'white' }}>
+                                    Ver
+                                </button>
+                            </td>
+                            <td>
+                                <button className="btn" style={{ backgroundColor: '#037250', color: 'white' }}>
+                                    Editar
+                                </button>
+                            </td>
+                            <td>Enviado</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </>
     );
