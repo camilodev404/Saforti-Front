7d283cbcd21e14263181ffa89ccceb1a01cb73d1
CamiloDev404 <crosti1826@gmail.com>
2024-03-21 18:29:16 -0500

    Cambio de posicion regreso ant

--- src/Components/Navbar/PrimeraFila.jsx
+++ src/Components/Navbar/PrimeraFila.jsx
@@ -6,7 +6,7 @@ export const PrimeraFila = () => {
             <a className="ant" href="https://www.ant.gov.co/">
                 <i className="material-icons">arrow_back</i>
             </a>
-            <h5>Regresar</h5>
+            <h5>Regresar Agencia Nacional de Tierras</h5>
         </div>
     );
 }
